146236e5fff0b277c22774d277100cf5b8d918df
Bhargav Kudipudi <kudipudibhargav@gmail.com>
2026-07-03 15:25:46 +0530

    fix: update favicon to B, add active Vercel project URLs, append DSA and OOP to technology grid, and resolve contact form submission by switching to native submission

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,35 +23,4 @@ $(document).ready(function(){
         $("#navIcon").html('<i class="fa-solid fa-bars-staggered"></i>');
     });
 
-    $('#contact_form').on('submit', function(e) {
-        e.preventDefault();
-        var form = $(this);
-        var submitButton = $("#contact_form_submit");
-        var statusDiv = $("#contact_form_status");
-
-        submitButton.addClass("disabled").prop("disabled", true);
-        submitButton.html("Sending...");
-        statusDiv.html("");
-
-        $.ajax({
-            method: 'POST',
-            url: form.attr('action'),
-            data: form.serialize(),
-            dataType: 'json',
-            headers: {
-                'Accept': 'application/json'
-            },
-            success: function(data) {
-                statusDiv.html("<p class='alert alert-success'>Thank you! Your message has been sent successfully. Formspree will request email activation on the first submission.</p>");
-                form.trigger("reset");
-                submitButton.removeClass("disabled").prop("disabled", false);
-                submitButton.html("Send message");
-            },
-            error: function(err) {
-                statusDiv.html("<p class='alert alert-danger'>Oops! There was a problem sending your message. Please try again.</p>");
-                submitButton.removeClass("disabled").prop("disabled", false);
-                submitButton.html("Send message");
-            }
-        });
-    });
 });
